12e09514a9c9f3471a132197b56426d66d75f70a
Ana Andric <aandric@z3r4p5.42lyon.fr>
2022-09-28 17:38:41 +0200

    adding gnl to personal git

---
+++ get_next_line.h
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aandric <aandric@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/01 12:18:40 by aandric           #+#    #+#             */
+/*   Updated: 2022/01/27 18:07:15 by aandric          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <fcntl.h>
+# include <limits.h>
+
+/* get_next_line.c */
+
+char	*get_next_line(int fd);
+char	*get_line(char *line, char *buffer, int fd);
+
+/* get_next_line_utils.c */
+
+int		ft_strlen(char *s);
+int		ft_chrn(char *s);
+int		ft_len_to_malloc(char *s1, char *s2);
+char	*ft_strjoin(char *s1, char *s2);
+void	ft_save_rest(char *buffer, char *shifted_buffer);
+
+#endif
